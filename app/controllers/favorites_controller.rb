0a5aab2bdfb225b925fde3ee2cd91c644a99c80f
DDDlucky <d.ono2105@gmail.com>
2021-04-10 19:46:10 +0900

    first commit

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id, user_id: current_user.id)
+    favorite.destroy
+  end
+end
